464cee2aa5e03283aa45e91b8623073d04485719
patriciagivort <63522907+patriciagvrt@users.noreply.github.com>
2025-02-06 15:36:35 -0300

    .

---
+++ app.js
@@ -0,0 +1,37 @@
+var myImage=document.querySelector("#imageid");
+function quadraImage()
+{
+    
+    myImage.src="imagens/ginasio.png";
+
+}
+function salaImage()
+{
+    myImage.src="imagens/salas.png";
+
+}
+
+function playgroundImage()
+{
+    myImage.src="imagens/playground.png";
+}
+function hortaImage()
+{
+    myImage.src="imagens/horta.png";
+}
+function monitoramentoImage()
+{
+    myImage.src="imagens/monitoramento.png";
+}
+function labImage()
+{
+    myImage.src="imagens/laboratorio.png";
+}
+function balletImage()
+{
+    myImage.src="imagens/ballet.png";
+}
+function leituraImage()
+{
+    myImage.src="imagens/integral.png";
+}
